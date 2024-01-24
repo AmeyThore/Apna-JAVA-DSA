e2fec5d56dfc6ed6485416aff77ed706c550db6f
Amey Thore <ameythore5@gmail.com>
2024-01-25 00:57:01 +0530

    Update Prime.java
    added optimized methods to get prime no.

--- Prime.java
+++ Prime.java
@@ -1,16 +1,51 @@
 public class Prime {
+    // type 1
 
-    public static boolean isPrime(int n){
+    // public static boolean isPrime(int n){
 
-        boolean isPrime = true;
+    //     boolean isPrime = true;
+    //     for(int i = 2; i <= n-1; i++){
+    //     if(n % i == 0){
+    //         isPrime = false;
+    //     }
+    // }
+    //     return isPrime;
+    // }
+
+
+    //type 2
+
+//     public static boolean isPrime(int n){
+
         
-        if(n % 2 == 0){
-            isPrime = false;
+//         if(n == 2){
+//             return true;
+//         }
+//         for(int i = 2; i <= n-1; i++){
+//         if(n % i == 0){
+//             return false;
+//         }
+//     }
+//         return true;
+//     }
+//type 3
+
+
+public static boolean isPrime(int n){
+    if(n == 2){
+        return true;
+    }
+
+    for(int i = 2;i <= Math.sqrt(n);i++ ){
+        if(n % i == 0){
+            return false;
         }
-        return isPrime;
     }
-    public static void main(String[] args) {
-        System.out.println(isPrime(12));;
-    }
-    
+    return true;
+}
+   
+public static void main(String[] args) {
+    System.out.println(isPrime(8));;
+}
+
 }
