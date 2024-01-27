7617421c8774bc83edc96de6dff92f21469175bb
Amey Thore <ameythore5@gmail.com>
2024-01-27 23:26:50 +0530

    Add files via upload
    Prime no's in a range 1 to n..

---
+++ PrimeRange.java
@@ -0,0 +1,27 @@
+public class PrimeRange {
+
+    public static boolean isPrime(int n){
+        if(n == 2){
+            return true;
+        }
+    
+        for(int i = 2;i <= Math.sqrt(n);i++ ){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void PrimeinRange(int n){
+        for(int i=2; i<=n; i++){
+            if(isPrime(i)){
+                System.out.print(i+" ");
+            }
+        }
+       System.out.println("");
+    } 
+    public static void main(String[] args) {
+        PrimeinRange(100);
+    }
+}
