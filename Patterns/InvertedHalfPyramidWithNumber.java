21bae5fd5b9c21b3f31fbb0982864f9122c957bb
Amey Thore <ameythore5@gmail.com>
2024-01-30 00:09:18 +0530

    Add files via upload
    inverted Half pyramid with the numbers

---
+++ Patterns/InvertedHalfPyramidWithNumber.java
@@ -0,0 +1,19 @@
+package Patterns;
+
+public class InvertedHalfPyramidWithNumber {
+
+    public static void Py(int n){
+
+        for(int i =1 ; i <= n; i++){
+
+            for(int j=1; j<= n-i+1; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Py(5);
+    }
+    
+}
