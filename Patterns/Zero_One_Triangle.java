afc9fef21458d81da53bf023b3c2c1f3d44b5f9b
Amey Thore <ameythore5@gmail.com>
2024-01-31 18:39:48 +0530

    Add files via upload
    Trangla made of 0's and 1's.

---
+++ Patterns/Zero_One_Triangle.java
@@ -0,0 +1,26 @@
+package Patterns;
+
+public class Zero_One_Triangle {
+
+    public static void zero_one_trangle(int n){
+
+        for(int i =1 ; i <= n; i++){
+
+            for(int j = 1; j<= i; j++){
+
+                if((i+j) % 2 == 0){
+                    System.out.print("1");
+                }
+                if((i+j) % 2 != 0){
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+        zero_one_trangle(5);
+    }
+}
