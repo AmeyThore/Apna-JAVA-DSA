61bbe9c7637ce82e46c968fafb02ae9ff86f21f6
Amey Thore <ameythore5@gmail.com>
2024-02-01 00:23:20 +0530

    Add files via upload
    hollow rhombus pattern of star

---
+++ Patterns/hollow_Rhombus.java
@@ -0,0 +1,26 @@
+package Patterns;
+
+public class hollow_Rhombus {
+    public static void hollow_rhombus(int n){
+        //outer 
+        for(int i =1; i <= n ;i++){
+
+            for(int j =1; j <= (n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j =1; j <= n; j++){
+                if(i == 1 || j == 1 ||  j == n || i == n){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }    
+    }
+    
+    public static void main(String args[]){
+        hollow_rhombus(5);
+    }
+    
+}
