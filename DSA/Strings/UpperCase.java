435a5b7a5eba7459f6fd41a2233940c5a09c1111
Amey Thore <ameythore5@gmail.com>
2024-02-25 00:44:23 +0530

    toUppercase function
    Que = make the first letter of the String Capital i.e UpperCase

---
+++ DSA/Strings/UpperCase.java
@@ -0,0 +1,25 @@
+package DSA.Strings;
+
+public class UpperCase {
+    public static String  upperCase(String str) {
+        StringBuilder sb = new StringBuilder("");
+        char ch =  Character.toUpperCase(str.charAt(0)); // to get the uppercase of 'a' which is A
+        sb.append(ch);
+
+        for(int i = 1; i<str.length(); i++){
+            if(str.charAt(i) == ' ' && i<str.length()-1){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i))); // to get the uppercase of ' ' which is ' '
+            }else{
+                sb.append(str.charAt(i));
+            }
+
+        }
+        return  sb.toString();
+    }
+    public static void main(String[] args) {
+        String str = "i am batman";
+        System.out.println(upperCase(str));
+    }
+}
