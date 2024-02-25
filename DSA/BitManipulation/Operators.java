0e7ef2d763fb777a588bb1ce85333c87ab9e4dc7
Amey Thore <ameythore5@gmail.com>
2024-02-26 00:07:10 +0530

    Operators in the Bit manipulation

---
+++ DSA/BitManipulation/Operators.java
@@ -0,0 +1,19 @@
+package DSA.BitManipulation;
+
+public class Operators {
+    public static void main(String[] args) {
+        //AND operator
+        System.out.println(5 & 3);   //1
+
+        //OR operator
+        System.out.println(5 | 3);   //7
+
+        //XOR operator
+        System.out.println(5 ^ 3);   //6
+
+        //One's compliment i.e not  operator
+        System.out.println(~5);      //-6
+
+        
+    }
+}
