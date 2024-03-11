435a1dc08533e82cb39e33dff68feabb11e39ef8
Amey Thore <ameythore5@gmail.com>
2024-03-11 23:47:19 +0530

    Static keyword in java

---
+++ OOPS/Static.java
@@ -0,0 +1,19 @@
+package OOPS;
+
+public class Static {
+
+    String name;
+    Static String
+    private java.lang.String SchoolName; SchoolName;
+
+    void setname(String name){
+        this.name=name;   
+    }
+    String getname(){
+        return this.name;
+    }
+    public static void main(String[] args) {
+        Static S1 = new Static();
+        S1.SchoolName="NVM";
+    }
+}
