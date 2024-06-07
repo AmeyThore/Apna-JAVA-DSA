71c387e5b22a7910e59b5e2f2f0865f2e86994fe
Amey Thore <ameythore5@gmail.com>
2024-06-07 23:50:01 +0530

    n queens in the row

---
+++ DSA/Backtracking/nQueens.java
@@ -0,0 +1,39 @@
+package DSA.Backtracking;
+
+public class nQueens {
+
+    public static void nqueens(char board[][], int row){
+        //base
+        if(row == board.length){
+            printBoard(board);
+            return;
+        }
+        //plasing queens
+        for(int j = 0; j < board.length; j++){
+            board[row][j] = 'Q';
+            nqueens(board, row+1);
+            board[row][j] = 'X';
+        }
+    }
+    public static void printBoard(char board[][]){
+        System.out.println("------------ chess board -----------");
+        for(int i = 0; i< board.length; i++){
+            for(int j = 0; j<board.length; j++){
+                System.out.print(board[i][j] + " ");
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int n =2;
+        char board[][] = new char[n][n];
+
+        for(int i = 0; i<n;i++){
+            for(int j = 0; j < n; j++){
+                board[i][j] = 'X';
+            }
+        }
+        nqueens(board, 0);
+
+    }
+}
