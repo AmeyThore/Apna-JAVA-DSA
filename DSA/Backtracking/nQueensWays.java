7fe608a68de254b6789cdb52666d5bb879cde4a2
Amey Thore <ameythore5@gmail.com>
2024-06-09 23:34:22 +0530

    no. of ways nQueens can be solved

---
+++ DSA/Backtracking/nQueensWays.java
@@ -0,0 +1,69 @@
+package DSA.Backtracking;
+
+public class nQueensWays {
+    public static boolean isSafe(char board[][],int row, int col){
+        //vertical up
+        for(int i=row-1; i>=0; i--){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+        //diagonal right
+        for(int i=row-1, j = col-1; i>=0 && j>=0; i--, j-- ){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        //diagonal left up
+        for(int i=row-1, j = col+1; i>=0 && j<board.length; i--, j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void nqueens(char board[][], int row){
+        //base
+        if(row == board.length){
+            count++;
+            // printBoard(board);
+            return;
+        }
+        //plasing queens
+        for(int j = 0; j < board.length; j++){
+            if(isSafe(board, row, j)){
+                board[row][j] = 'Q';
+                nqueens(board, row+1);
+                board[row][j] = 'X';
+            }
+            
+        }
+    }
+    public static void printBoard(char board[][]){
+        System.out.println("------------ chess board -----------");
+
+        for(int i = 0; i< board.length; i++){
+            for(int j = 0; j<board.length; j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    static int count = 0;
+    public static void main(String[] args) {
+        int n = 11;
+        char board[][] = new char[n][n];
+
+        for(int i = 0; i<n;i++){
+            for(int j = 0; j < n; j++){
+                board[i][j] = 'X';
+            }
+        }
+        nqueens(board, 0);
+
+        System.out.println("no. of ways nQueens can be solved = "+ count);
+
+    }
+}
