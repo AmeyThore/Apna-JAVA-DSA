eb13a5ae952e86cffef5c253bd6e8722034ff298
Amey Thore <ameythore5@gmail.com>
2024-06-15 23:13:19 +0530

    Reverse of an array list

---
+++ DSA/ArrayList/ReverseArray.java
@@ -0,0 +1,20 @@
+package DSA.ArrayList;
+
+import java.util.ArrayList;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        ArrayList <Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        for(int i = list.size()-1; i >= 0; i--){
+            System.out.println(list.get(i));
+        }
+
+    }
+}
