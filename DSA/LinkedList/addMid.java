788b6d8544d4b2ae6a96b51041f2b2ff753cebd4
Amey Thore <ameythore5@gmail.com>
2024-06-27 23:47:49 +0530

    add the element in the middle of the LL
    O(1) = tc

---
+++ DSA/LinkedList/addMid.java
@@ -0,0 +1,81 @@
+package DSA.LinkedList;
+
+public class addMid{
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+    public static Node head;
+    public static Node tail;
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+
+        }
+        newNode.next =head;
+        head = newNode;
+    }
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+    public void addMid(int idx, int data){
+        if ( idx == 0){
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+
+        while(i < idx-1){
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+        
+    }
+    public void print(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data+"->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args) {
+       addMid ll = new addMid();
+        ll.print();
+        ll.addFirst(2);
+        ll.print();
+
+        ll.addFirst(1);
+        ll.print();
+
+        ll.addLast(3);
+        ll.print();
+
+        ll.addLast(4);
+        ll.print();
+
+        ll.addMid(3, 9);
+        ll.print();
+        
+    }
+
+
+}
