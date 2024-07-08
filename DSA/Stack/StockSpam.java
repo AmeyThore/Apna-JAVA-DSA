90c09c5291ae1f771e182547d9cfce972e547e99
Amey Thore <ameythore5@gmail.com>
2024-07-08 23:53:33 +0530

    Find the prev high of stock in O(n)

---
+++ DSA/Stack/StockSpam.java
@@ -0,0 +1,12 @@
+package DSA.Stack;
+
+public class StockSpam {
+    public static void main(String[] args) {
+        int[] Stocks = {100, 80, 60, 70, 60, 85, 100};
+        int[] span = new int[Stocks.length];
+
+        for(int i =0;  i<span.length; i++){
+            System.out.println(span[i]+ " ");
+        }
+    }
+}
