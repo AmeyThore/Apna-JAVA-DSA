20fdfaee0108c50929f9ebe685ecce2456caa6f2
Amey Thore <ameythore5@gmail.com>
2024-07-11 00:31:33 +0530

    Update ValidParenthesis.java

--- DSA/Stack/ValidParenthesis.java
+++ DSA/Stack/ValidParenthesis.java
@@ -1,5 +1,39 @@
 package DSA.Stack;
+import java.util.Stack;
 
 public class ValidParenthesis {
-    
+    public static boolean isValid(String str){
+        Stack<Character> s = new Stack<>();
+        for(int i = 0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            //opening
+            if(ch == '(' || ch == '{' || ch == '['){
+                s.push(ch);
+            }else{
+                //closing
+                if(s.isEmpty()){
+                    return false;
+                }else{
+                    if((s.peek() == '(' && ch == ')') 
+                    || (s.peek() == '{' && ch == '}')
+                    || (s.peek() == '[' && ch == ']')){
+                        s.pop();
+                    }else{
+                        return false;
+                    }
+                    
+                }
+
+            }
+        }
+        if(s.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        String str = "()[]()";
+        System.out.println(isValid(str));
+    }
 }
